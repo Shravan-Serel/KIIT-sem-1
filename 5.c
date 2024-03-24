3c8cf267354abee28d2dfdbafb1c32692f373037
Shravan Serel <118686085+Shravan-Serel@users.noreply.github.com>
2024-03-24 13:44:38 +0530

    Updated description
    concatenate two strings

--- 5.c
+++ 5.c
@@ -5,15 +5,15 @@
 
 int main() {
     char str1[MAX_LENGTH], str2[MAX_LENGTH];
-    char concatenated[MAX_LENGTH * 2]; // Double the maximum length to accommodate both strings
+    char concatenated[MAX_LENGTH * 2];
 
     printf("Enter the first string: ");
     fgets(str1, MAX_LENGTH, stdin);
-    str1[strcspn(str1, "\n")] = '\0'; // Removing newline character
+    str1[strcspn(str1, "\n")] = '\0';
 
     printf("Enter the second string: ");
     fgets(str2, MAX_LENGTH, stdin);
-    str2[strcspn(str2, "\n")] = '\0'; // Removing newline character
+    str2[strcspn(str2, "\n")] = '\0';
 
     strcpy(concatenated, str1);
     strcat(concatenated, str2);
